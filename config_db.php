88504ef6e557a004220850cc363789b1c6dde8d7
Natthaphong1996 <nattaphong.meesawad@gmail.com>
2025-06-25 08:49:05 +0700

    Update config_db.php

--- config_db.php
+++ config_db.php
@@ -3,7 +3,7 @@
 $servername = "localhost"; // ชื่อเซิร์ฟเวอร์ (เช่น localhost)
 $username = "root"; // ชื่อผู้ใช้ฐานข้อมูล
 $password = ""; // รหัสผ่านของผู้ใช้ฐานข้อมูล (ถ้าว่างใส่ "")
-$dbname = "prod_bf"; // ชื่อฐานข้อมูลที่คุณใช้
+$dbname = "prod_bf_p2"; // ชื่อฐานข้อมูลที่คุณใช้
 
 // สร้างการเชื่อมต่อฐานข้อมูล
 $conn = new mysqli($servername, $username, $password, $dbname);
